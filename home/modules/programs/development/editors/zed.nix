6f39da243fc0c117c380f3ca042ecf4ec1f127b7
minusKou <32330840+minusKou@users.noreply.github.com>
2026-08-30 17:26:31 +0800

    feat(editor): add alejandra as formatter for nix files

--- home/modules/programs/development/editors/zed.nix
+++ home/modules/programs/development/editors/zed.nix
@@ -19,6 +19,18 @@
         };
       };
 
+      # Use Alejandra as the Nix formatter
+      languages = {
+        Nix = {
+          formatter = {
+            external = {
+              command = "alejandra";
+              arguments = [ "--quiet" "-" ];
+            };
+          };
+        };
+      };
+
       # Font configurations (optional)
       ui_font_family = "Readex Pro";
       buffer_font_family = "JetBrainsMono Nerd Font";
@@ -35,6 +47,7 @@
 
   # NixOS related fixes
   home.packages = with pkgs; [
+    alejandra # .nix formatter
     nixd
     nil
   ];
