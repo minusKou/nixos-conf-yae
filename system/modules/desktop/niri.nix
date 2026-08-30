732275b3bf9c614bd87f3193c978c940be9ec0e9
minusKou <32330840+minusKou@users.noreply.github.com>
2026-08-30 17:28:13 +0800

    fix(pkgs): correct tuigreet package

--- system/modules/desktop/niri.nix
+++ system/modules/desktop/niri.nix
@@ -21,7 +21,7 @@
 
         # Fallback / Logout screen using tuigreet
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-user-session --asterisks --cmd niri-session";
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-user-session --asterisks --cmd niri-session";
           user = "greeter";
         };
       };
