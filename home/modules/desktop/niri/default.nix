2984650afd0df9970985cde347e32b1838f983db
minusKou <32330840+minusKou@users.noreply.github.com>
2026-08-31 01:22:29 +0800

    refactor: change from folder structure to mutable niri custom config

--- home/modules/desktop/niri/default.nix
+++ home/modules/desktop/niri/default.nix
@@ -4,11 +4,20 @@
   xdg.configFile = {
     "niri/config.kdl".source = ./config.kdl;
     "niri/niri".source = ./niri;
-    "niri/custom".source = ./custom;
     "niri/bg.png".source = ./bg.png;
   };
 
-  home.packages = with pkgs; [    
+  # User-level Niri configuration
+  systemd.user.tmpfiles.rules = [
+    "d %h/.config/niri/custom 0755 - - - -"
+    "f %h/.config/niri/custom/env.kdl 0644 - - - -"
+    "f %h/.config/niri/custom/general.kdl 0644 - - - -"
+    "f %h/.config/niri/custom/keybinds.kdl 0644 - - - -"
+    "f %h/.config/niri/custom/rules.kdl 0644 - - - -"
+    "f %h/.config/niri/custom/startup.kdl 0644 - - - -"
+  ];
+
+  home.packages = with pkgs; [
     # Dolphin File Manager
     kdePackages.dolphin
     kdePackages.kio
