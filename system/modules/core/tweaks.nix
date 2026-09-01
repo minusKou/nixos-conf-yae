e88f2980c1ff5c3bfa26722cc34a5e275f5ae740
minusKou <32330840+minusKou@users.noreply.github.com>
2026-09-01 15:41:39 +0800

    refactor(pkgs): use git version of ananicy-rules-cachyos

--- system/modules/core/tweaks.nix
+++ system/modules/core/tweaks.nix
@@ -11,7 +11,7 @@
   services.ananicy = {
     enable = true;
     package = pkgs.ananicy-cpp;
-    rulesProvider = pkgs.ananicy-rules-cachyos;
+    rulesProvider = pkgs.ananicy-rules-cachyos_git;
   };
 
   # Kernel Sysctl Tweaks (CachyOS Defaults)
